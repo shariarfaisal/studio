444762fac125b5976a1743fb3979a532df248ca3
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-11-13 12:31:41 +0600

    feat: extend Project interface with BaseTypes

--- src/types/project/index.ts
+++ src/types/project/index.ts
@@ -1,15 +1,13 @@
+import { BaseTypes } from "../base";
 import { Source } from "../source";
 import { Topic } from "../topic";
 
 // Project.ts
-export interface Project {
-  id: string;
+export interface Project extends BaseTypes {
   name: string;
   systemPrompt: string;
   status: string;
   topics: Topic[];
   sources: Source[];
   configs: Record<string, string>; // Key-value pair for configs
-  createdAt: Date;
-  updatedAt: Date;
 }
