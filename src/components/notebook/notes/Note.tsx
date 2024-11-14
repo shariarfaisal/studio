1a05b7d1141529f1d84edd79813129b1edd9dadb
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-11-14 11:14:00 +0600

    feat: add custom hooks for editor and debounce functionality, implement Editor component for note editing

--- src/components/notebook/notes/Note.tsx
+++ src/components/notebook/notes/Note.tsx
@@ -1,37 +1,35 @@
-"use client";
+'use client';
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui";
-import Markdown from "react-markdown";
-import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui';
+import Markdown from 'react-markdown';
+
 import {
   Dialog,
   DialogContent,
   DialogTitle,
   DialogHeader,
   DialogTrigger,
-} from "../../ui/dialog";
-import { useNotebook } from "../Provider";
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { useToast } from "@/hooks/use-toast";
-import { Edit3 } from "lucide-react";
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
-import Heading from "@tiptap/extension-heading";
-import { Topic} from "@/types";
-import { useProjectStore } from "@/store";
-import { useParams } from "next/navigation";
+} from '../../ui/dialog';
+import { useNotebook } from '../Provider';
+import { useEffect, useRef, useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/hooks/use-toast';
+import { Edit3 } from 'lucide-react';
+import { Topic } from '@/types';
+import { useProjectStore } from '@/store';
+import { useParams } from 'next/navigation';
+import { useEditor } from '@/hooks';
+import { Editor } from './editor';
 
-function NoteTitle({ note,editable }: { note: Topic,editable:boolean }) {
-  const {id}=useParams()
+function NoteTitle({ note, editable }: { note: Topic; editable: boolean }) {
+  const { id } = useParams();
   const { updateNote } = useNotebook();
   const { toast } = useToast();
   const [edit, setEdit] = useState(false);
   const [isPending, setIsLoading] = useState(false);
   const [title, setTitle] = useState(note.name);
-  const {setProjectStore}=useProjectStore()
+  const { setProjectStore } = useProjectStore();
+
   // const { mutate, isPending } = useMutation({
   //   mutationKey: ["updateNote"],
   //   mutationFn: notebookService.updateNote,
@@ -49,14 +47,15 @@ function NoteTitle({ note,editable }: { note: Topic,editable:boolean }) {
   //   },
   // });
 
- 
   const onSubmit = () => {
     setIsLoading(true);
-    setProjectStore((prev) => {
-      const updatedProjects = prev.projects.map((proj) => {
-        if (proj.id === id as string) {
-          const updatedTopics = proj.topics.map((topic) =>
-            topic.id === note.id ? { ...topic, name: title,data: note.data} : topic
+    setProjectStore(prev => {
+      const updatedProjects = prev.projects.map(proj => {
+        if (proj.id === (id as string)) {
+          const updatedTopics = proj.topics.map(topic =>
+            topic.id === note.id
+              ? { ...topic, name: title, data: note.data }
+              : topic
           );
           return {
             ...proj,
@@ -76,19 +75,18 @@ function NoteTitle({ note,editable }: { note: Topic,editable:boolean }) {
     setEdit(false);
   };
 
-
   if (edit && editable) {
     return (
       <Input
-        className="w-[calc(100%-40px)]"
+        className='w-[calc(100%-40px)]'
         value={title}
         disabled={isPending}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
+        onKeyDown={e => {
+          if (e.key === 'Enter') {
             onSubmit();
           }
         }}
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={e => setTitle(e.target.value)}
       />
     );
   }
@@ -102,7 +100,7 @@ function NoteTitle({ note,editable }: { note: Topic,editable:boolean }) {
           }
         }}
         className={`w-[calc(100%-40px)] text-lg font-medium ${
-          editable ? "cursor-pointer hover:opacity-80" : ""
+          editable ? 'cursor-pointer hover:opacity-80' : ''
         }`}
       >
         {note.name}
@@ -112,38 +110,45 @@ function NoteTitle({ note,editable }: { note: Topic,editable:boolean }) {
 }
 
 type NoteProps = {
-  note: Topic,
-  selected: boolean
+  note: Topic;
+  selected: boolean;
 };
 
-export const Note = ({note,selected}:NoteProps) => {
+export const Note = ({ note, selected }: NoteProps) => {
   const { toggleSelectNote } = useNotebook();
-  const [editable, setEditable] = useState(true)
+  const { id } = useParams();
+  const { setProjectStore } = useProjectStore();
+  const [editable, setEditable] = useState(true);
+  const [value, setValue] = useState(note.data);
+  const handleBlur = () => {
+    setProjectStore(prev => {
+      const updatedProjects = prev.projects.map(proj => {
+        if (proj.id === (id as string)) {
+          const updatedTopics = proj.topics.map(topic =>
+            topic.id === note.id ? { ...topic, data: value } : topic
+          );
+          return {
+            ...proj,
+            topics: updatedTopics,
+          };
+        }
+        return proj;
+      });
 
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Document,
-      Paragraph,
-      Text,
-      Heading.configure({
-        levels: [1, 2, 3],
-      }),
-    ],
-    content: note.data,
-    editable: editable,
-    autofocus: true,
-    injectCSS: false,
-    onUpdate: () => {},
-  });
+      return {
+        ...prev,
+        projects: updatedProjects,
+      };
+    });
+  };
 
   return (
-    <Card className="max-h-[300px] overflow-hidden shadow-none border-2 border-gray-200">
-      <CardHeader className="w-full bg-background flex flex-row justify-between items-start">
-        <div className="w-full flex flex-col">
+    <Card className='max-h-[300px] overflow-hidden shadow-none border-2 border-gray-200'>
+      <CardHeader className='w-full bg-background flex flex-row justify-between items-start'>
+        <div className='w-full flex flex-col'>
           {note && editable && (
-            <div className="flex gap-2 items-center text-zinc-600 text-sm">
-              <Edit3 className="w-4 h-4" />
+            <div className='flex gap-2 items-center text-zinc-600 text-sm'>
+              <Edit3 className='w-4 h-4' />
               <span>Edit Note</span>
             </div>
           )}
@@ -152,32 +157,33 @@ export const Note = ({note,selected}:NoteProps) => {
         <input
           id={`note-${note.id}`}
           style={{
-            width: "20px",
-            height: "20px",
+            width: '20px',
+            height: '20px',
           }}
-          type="checkbox"
-          onChange={(e) => {
+          type='checkbox'
+          onChange={e => {
             toggleSelectNote(note.id, e.target.checked);
           }}
           checked={selected}
         />
       </CardHeader>
-
-      <Dialog>
+      <Dialog modal={true}>
         <DialogTrigger asChild>
-          <CardContent className="text-sm partial-text h-[200px] cursor-pointer">
-            <Markdown>{note.data}</Markdown>
+          <CardContent className='text-sm partial-text min-h-[200px] cursor-pointer'>
+            <div dangerouslySetInnerHTML={{ __html: note.data }} />
           </CardContent>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-2xl">
+        <DialogContent className='sm:max-w-2xl'>
           <DialogHeader>
             <DialogTitle>{note.name}</DialogTitle>
           </DialogHeader>
-          <div className="text-sm min-h-[200px] max-h-[400px] overflow-x-hidden scroll-y text-wrap">
+          <div>
             {editable ? (
-              <EditorContent editor={editor} />
+              <Editor value={value} onChange={setValue} onBlur={handleBlur} />
             ) : (
-              <Markdown className="text-wrap">{note.data}</Markdown>
+              <div className='text-sm  min-h-[200px] max-h-[400px] overflow-x-hidden scroll-y text-wrap'>
+                <div dangerouslySetInnerHTML={{ __html: note.data }} />
+              </div>
             )}
           </div>
         </DialogContent>
@@ -185,5 +191,3 @@ export const Note = ({note,selected}:NoteProps) => {
     </Card>
   );
 };
-
-
