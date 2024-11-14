03247d57055d1647afdf5ee9bcbb5a74a1114657
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-11-14 12:34:39 +0600

    refactor: standardize import paths and update quotes to single quotes

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,6 @@
-"use client";
-import NotebookProvider from "@/components/notebook/provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+'use client';
+import NotebookProvider from '@/components/notebook/temp/provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
--- src/components/notebook/sidebar/notebook-sidebar.tsx
+++ src/components/notebook/sidebar/notebook-sidebar.tsx
@@ -1,40 +1,40 @@
-"use client";
-import { FileText } from "lucide-react";
+'use client';
+import { FileText } from 'lucide-react';
 
-import { useNotebook } from "../provider";
-import { useEffect, useState } from "react";
-import ContentLoader from "react-content-loader";
-import ChatHistory from "./chat-history";
-import { SidebarMenu, SidebarMenuItem } from "@/components/ui";
-import { useProjectStore } from "@/store";
-import { useParams } from "next/navigation";
-import { AddSource } from "./add-source";
+import { useNotebook } from '../provider';
+import { useEffect, useState } from 'react';
+import ContentLoader from 'react-content-loader';
+import ChatHistory from './chat-history';
+import { SidebarMenu, SidebarMenuItem } from '@/components/ui';
+import { useProjectStore } from '@/store';
+import { useParams } from 'next/navigation';
+import { AddSource } from './add-source';
 
 function LoadingItems() {
   return (
     <ContentLoader
-      viewBox="0 0 230 300" // Adjusted to fit all items
-      width="100%"
-      height="auto"
-      backgroundColor="#c3c3c3"
-      foregroundColor="#fcfcfc"
-      className=""
+      viewBox='0 0 230 300' // Adjusted to fit all items
+      width='100%'
+      height='auto'
+      backgroundColor='#c3c3c3'
+      foregroundColor='#fcfcfc'
+      className=''
     >
-      <rect x="0" y="15" rx="3" ry="3" width="20" height="20" />
-      <rect x="30" y="15" rx="3" ry="3" width="150" height="20" />
-      <rect x="200" y="15" rx="3" ry="3" width="20" height="20" />
+      <rect x='0' y='15' rx='3' ry='3' width='20' height='20' />
+      <rect x='30' y='15' rx='3' ry='3' width='150' height='20' />
+      <rect x='200' y='15' rx='3' ry='3' width='20' height='20' />
 
-      <rect x="0" y="50" rx="3" ry="3" width="20" height="20" />
-      <rect x="30" y="50" rx="3" ry="3" width="150" height="20" />
-      <rect x="200" y="50" rx="3" ry="3" width="20" height="20" />
+      <rect x='0' y='50' rx='3' ry='3' width='20' height='20' />
+      <rect x='30' y='50' rx='3' ry='3' width='150' height='20' />
+      <rect x='200' y='50' rx='3' ry='3' width='20' height='20' />
 
-      <rect x="0" y="85" rx="3" ry="3" width="20" height="20" />
-      <rect x="30" y="85" rx="3" ry="3" width="150" height="20" />
-      <rect x="200" y="85" rx="3" ry="3" width="20" height="20" />
+      <rect x='0' y='85' rx='3' ry='3' width='20' height='20' />
+      <rect x='30' y='85' rx='3' ry='3' width='150' height='20' />
+      <rect x='200' y='85' rx='3' ry='3' width='20' height='20' />
 
-      <rect x="0" y="120" rx="3" ry="3" width="20" height="20" />
-      <rect x="30" y="120" rx="3" ry="3" width="150" height="20" />
-      <rect x="200" y="120" rx="3" ry="3" width="20" height="20" />
+      <rect x='0' y='120' rx='3' ry='3' width='20' height='20' />
+      <rect x='30' y='120' rx='3' ry='3' width='150' height='20' />
+      <rect x='200' y='120' rx='3' ry='3' width='20' height='20' />
     </ContentLoader>
   );
 }
@@ -67,26 +67,26 @@ export const NotebookSidebar = () => {
   }, [project]);
 
   return (
-    <div className="p-2.5 flex flex-col gap-3">
+    <div className='p-2.5 flex flex-col gap-3'>
       <ChatHistory />
-      <div className="flex items-center justify-between p-2.5 bg-background rounded-lg shadow-md">
-        <h3 className="text-base font-semibold">Add Source</h3>
+      <div className='flex items-center justify-between p-2.5 bg-background rounded-lg shadow-md'>
+        <h3 className='text-base font-semibold'>Add Source</h3>
         <AddSource />
       </div>
-      <SidebarMenu className="bg-background p-2.5 pr-1 rounded-lg shadow-md">
-        <div className="flex items-center justify-between pr-2">
+      <SidebarMenu className='bg-background p-2.5 pr-1 rounded-lg shadow-md'>
+        <div className='flex items-center justify-between pr-2'>
           <div>
-            <h3 className="font-semibold">Sources</h3>
-            <p className="text-gray-500">
+            <h3 className='font-semibold'>Sources</h3>
+            <p className='text-gray-500'>
               {sources.selected.length}/{sources.data.length} selected
             </p>
           </div>
           <div>
             <input
-              type="checkbox"
-              className="w-4 h-4 cursor-pointer"
+              type='checkbox'
+              className='w-4 h-4 cursor-pointer'
               checked={isAllSourcesSelected}
-              onChange={(e) => {
+              onChange={e => {
                 if (e.target.checked) {
                   selectAllSources();
                 } else {
@@ -96,21 +96,21 @@ export const NotebookSidebar = () => {
             />
           </div>
         </div>
-        <div className=" flex flex-col gap-2 max-h-[15rem] scroll-y pr-2">
-          {project?.sources?.map((item) => (
+        <div className=' flex flex-col gap-2 max-h-[15rem] scroll-y pr-2'>
+          {project?.sources?.map(item => (
             <SidebarMenuItem
               key={item.id}
               onClick={() => openSourceDetails(item.file)}
-              className="rounded-xl h-12 flex justify-between items-center transition-all duration-150 ease-in-out"
+              className='rounded-xl h-12 flex justify-between items-center transition-all duration-150 ease-in-out'
             >
-              <div className="h-full flex items-center gap-2 cursor-pointer md:text-base font-medium hover:opacity-80 hover:underline">
-                <FileText className="w-4 h-4" />
-                <span className="">{item.file.filename}</span>
+              <div className='h-full flex items-center gap-2 cursor-pointer md:text-base font-medium hover:opacity-80 hover:underline'>
+                <FileText className='w-4 h-4' />
+                <span className=''>{item.file.filename}</span>
               </div>
               <input
-                type="checkbox"
+                type='checkbox'
                 checked={sources.selected.includes(item.id)}
-                className="w-4 h-4 cursor-pointer"
+                className='w-4 h-4 cursor-pointer'
                 onChange={() => toggleSource(item.id)}
               />
             </SidebarMenuItem>
